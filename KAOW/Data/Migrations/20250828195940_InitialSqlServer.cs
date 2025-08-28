2d89f33b837dc170cbf845a834a53031b04e71fd
vinibanciela <vinibanciela@gmail.com>
2025-08-28 17:16:05 -0300

    reoorganizando pasta migrations para dentro da Data, e renomeando namespace

---
+++ KAOW/Data/Migrations/20250828195940_InitialSqlServer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KAOW.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSqlServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventosExtremos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Local = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventosExtremos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instituicoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CNPJ = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instituicoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BasesEmergencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Localizacao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstituicaoId = table.Column<int>(type: "int", nullable: true),
+                    EventoExtremoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasesEmergencias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BasesEmergencias_EventosExtremos_EventoExtremoId",
+                        column: x => x.EventoExtremoId,
+                        principalTable: "EventosExtremos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_BasesEmergencias_Instituicoes_InstituicaoId",
+                        column: x => x.InstituicaoId,
+                        principalTable: "Instituicoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventoInstituicoes",
+                columns: table => new
+                {
+                    EventoExtremoId = table.Column<int>(type: "int", nullable: false),
+                    InstituicaoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventoInstituicoes", x => new { x.EventoExtremoId, x.InstituicaoId });
+                    table.ForeignKey(
+                        name: "FK_EventoInstituicoes_EventosExtremos_EventoExtremoId",
+                        column: x => x.EventoExtremoId,
+                        principalTable: "EventosExtremos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventoInstituicoes_Instituicoes_InstituicaoId",
+                        column: x => x.InstituicaoId,
+                        principalTable: "Instituicoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasesEmergencias_EventoExtremoId",
+                table: "BasesEmergencias",
+                column: "EventoExtremoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasesEmergencias_InstituicaoId",
+                table: "BasesEmergencias",
+                column: "InstituicaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventoInstituicoes_InstituicaoId",
+                table: "EventoInstituicoes",
+                column: "InstituicaoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BasesEmergencias");
+
+            migrationBuilder.DropTable(
+                name: "EventoInstituicoes");
+
+            migrationBuilder.DropTable(
+                name: "EventosExtremos");
+
+            migrationBuilder.DropTable(
+                name: "Instituicoes");
+        }
+    }
+}
